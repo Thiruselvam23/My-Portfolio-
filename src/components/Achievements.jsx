702309c6d5b6483686454be9704a3470c0ba4879
Thiruselvam P <thiruselvampanneer@gmail.com>
2025-09-23 21:58:23 +0530

    Update Achievements.jsx

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -8,7 +8,7 @@ function Achievements() {
          <h3>ScholarShip</h3> SPURS in Shiv Nadar University, Batch: 2023-27.
           <h3>Problem Solving: </h3>Solved 100+ problems on LeetCode.
           <h3>Programming & CS Fundamentals: </h3> Programming in Java (Infosys), Data Structures & Algorithms using
-          Java (Infosys), Database Management Systems (NPTEL).
+          Java (Infosys), Database Management Systems (Infosys),Getting started with competitive programming(NPTEL), Introduction to Large Language model(NPTEL).
           <h3>Agile & Software Engineering:</h3> Agile Scrum in Practice (Infosys), Software Engineering and Agile Software
           Practices (Infosys).
           <h3>Databases & Systems:</h3> Introduction to NoSQL Databases (Infosys).
